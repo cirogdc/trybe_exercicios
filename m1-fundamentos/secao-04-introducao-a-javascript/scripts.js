f8885182aec51d6f2662febbad794112346cf170
cirogdc <cirogdc@gmail.com>
2022-11-23 19:09:57 -0300

    Para fixar - módulo 1, sessão 4, dia 1

---
+++ m1-fundamentos/secao-04-introducao-a-javascript/scripts.js
@@ -0,0 +1,77 @@
+    //Exercício 1
+// const myName = "Ciro Garcia Drago Couto";
+// const birthCity = "São Paulo";
+// let birthYear = "1991";
+
+// console.log(myName);
+// console.log(birthCity);
+// console.log(birthYear);
+
+    //Exercício 2
+// const base = 5;
+// const height = 8;
+// const area = base * height;
+// const perimeter = base + base + height + height;
+
+// console.log(area);
+// console.log(perimeter);
+
+    //Exercício 3
+// const notaPessoaCandidata = 52;
+
+// if (notaPessoaCandidata >= 80) {
+//     console.log("Parabéns, você foi aprovado(a)!");
+// } else if (notaPessoaCandidata >= 60 && notaPessoaCandidata < 80) {
+//     console.log("Você está na nossa lista de espera.");
+// } else {
+//     console.log("Você foi reprovado(a).");
+// }
+
+    //Exercício 4
+    //Parte 1
+// const currentHour = 15;
+// let message = "Você está dormindo";
+
+// if (currentHour > 22) {
+//     console.log ("Não deveríamos comer nada, é hora de dormir");
+// }
+// else if (currentHour >= 18 && currentHour < 22) {
+//     console.log ("Rango da noite, vamos jantar :D");
+// } else if (currentHour >= 14 && currentHour < 18) {
+//     console.log ("Vamos fazer um bolo pro café da tarde?");
+// } else if(currentHour >= 11 && currentHour < 14) {
+//     console.log ("Hora do almoço!!!");
+// } else if (currentHour >= 4 && currentHour < 11) {
+//     console.log ("Hmmm, cheiro de café recém passado!");
+// } else {
+//     console.log (message);
+// }
+
+    //Parte 2
+// let weekDay = "sábado"
+
+// if (weekDay === "segunda-feira" || weekDay === "terça-feira" || weekDay === "quarta-feira" || weekDay === "quinta-feira" || weekDay === "sexta-feira") {
+//     console.log ("Oba, mais um dia de aprendizado na Trybe >:D")
+// } else {
+//     console.log ("FINALMENTE, desanco merecido UwU")
+// }
+
+    //Exercício 5
+// let statusAprovacao = "reprovada";
+
+// switch (statusAprovacao) {
+//     case "aprovada":
+//         console.log ("Parabéns, você foi aprovado(a)!");
+//         break;
+
+//     case "lista":
+//         console.log ("Você está na nossa lista de espera.");
+//         break;
+
+//     case "reprovada":
+//         console.log ("Você foi reprovado(a).");
+//         break;
+    
+//     default: 
+//         console.log ("Informação incorreta.")
+// }
