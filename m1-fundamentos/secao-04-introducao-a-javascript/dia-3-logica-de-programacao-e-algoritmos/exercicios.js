2cc705b423651bea1b33762336f03d165b1a501d
cirogdc <cirogdc@gmail.com>
2022-11-29 15:35:37 -0300

    Exercício 1 resolvido

---
+++ m1-fundamentos/secao-04-introducao-a-javascript/dia-3-logica-de-programacao-e-algoritmos/exercicios.js
@@ -0,0 +1,12 @@
+    // Exercício 1
+// O fatorial é representado pelo sinal !
+// 4! = 4 x 3 x 2 x 1 = 24
+// Com base nessas informações, crie um algoritmo que imprima na tela o fatorial de 10.
+
+let fatorial = 1;
+
+for (let index = 10; index > 0; index -= 1) {
+    fatorial * index;
+}
+
+console.log(fatorial);
